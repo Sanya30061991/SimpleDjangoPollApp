9526357e17468b63fc0d634b15637a6f4f568e78
Alexander <youdoit@mail.ru>
2020-07-31 05:02:14 +0600

    Finished results and voting process

--- Main/urls.py
+++ Main/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('show', views.show, name = 'polls'),
-    path('logout', views.loggggg, name = 'logoff')
+    path('logout', views.loggggg, name = 'logoff'),
+    path('vote', views.vote, name = 'vote'),
+    path('results', views.res, name = 'results')
 ]
--- Main/views.py
+++ Main/views.py
@@ -3,6 +3,63 @@ from .models import Poll
 from .forms import PollForm
 from django.contrib.auth import logout
 # Create your views here.
+
+def res(request):
+    if request.method == "GET":
+        id = request.GET['Results']
+        poll = Poll.objects.get(id=id)
+        context = {
+            'poll':poll
+        }
+    return render(request, 'Main/result.html', context)
+
+def vote(request):
+    if request.method == "GET":
+        id = request.GET['Vote']
+        poll = Poll.objects.get(id=id)
+        context = {
+            'poll':poll,
+            'errors':[]
+        }
+        return render(request, 'Main/vote.html', context)
+    elif request.method == "POST":
+        context = {
+            'errors':[]
+        }
+        if "opt1" in request.POST:
+            id = request.POST['opt1']
+            poll = Poll.objects.get(id=id)
+            poll.votes1 = poll.votes1+1
+            poll.total = poll.total+1
+            poll.save()
+            context = {
+                'poll':poll
+            }
+            return render(request, 'Main/result.html', context)
+        elif "opt2" in request.POST:
+            id = request.POST['opt2']
+            poll = Poll.objects.get(id=id)
+            poll.votes2 = poll.votes2+1
+            poll.total = poll.total+1
+            poll.save()
+            context = {
+                'poll':poll
+            }
+            return render(request, 'Main/result.html', context)
+        elif "opt3" in request.POST:
+            id = request.POST['opt3']
+            poll = Poll.objects.get(id=id)
+            poll.votes3 = poll.votes3+1
+            poll.total = poll.total+1
+            poll.save()
+            context = {
+                'poll':poll
+            }
+            return render(request, 'Main/result.html', context)
+        else:
+            context['errors'].append("Vote before submitting!")
+            return render(request, 'Main/vote.html', context)
+
 def loggggg(request):
     logout(request)
     return redirect('logg')
